8b1dc9a8affca7ce75ba40ddc5aed1d5efe6a27c
FlanB <clement.renou@mmibordeaux.com>
2021-10-26 10:41:38 +0200

    ⚡️ better variables

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -13,7 +13,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private int maxEnemies;
 
-    private int spawnColumn;
+    private int spawnRow = 6;
 
     // Start is called before the first frame update
     void Start()
@@ -30,31 +30,31 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < maxEnemies; i++)
         {
-            spawnColumn = Random.Range(0, 4);
-
-            switch (spawnColumn)
+            switch (Random.Range(0, 4))
             {
                 case 0:
                     Instantiate(enemy,
-                    new Vector2(-2.25f, 5),
+                    new Vector2(-2.25f, spawnRow),
                     Quaternion.identity);
                     break;
                 case 1:
                     Instantiate(enemy,
-                    new Vector2(-1.15f, 5),
+                    new Vector2(-1.15f, spawnRow),
                     Quaternion.identity);
                     break;
                 case 2:
-                    Instantiate(enemy, new Vector2(0, 5), Quaternion.identity);
+                    Instantiate(enemy,
+                    new Vector2(0, spawnRow),
+                    Quaternion.identity);
                     break;
                 case 3:
                     Instantiate(enemy,
-                    new Vector2(1.15f, 5),
+                    new Vector2(1.15f, spawnRow),
                     Quaternion.identity);
                     break;
                 case 4:
                     Instantiate(enemy,
-                    new Vector2(2.25f, 5),
+                    new Vector2(2.25f, spawnRow),
                     Quaternion.identity);
                     break;
             }
