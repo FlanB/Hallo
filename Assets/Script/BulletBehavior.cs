c21cba2db34c83a05cd65b9c724567152b8e3636
FlanB <clement.renou@mmibordeaux.com>
2021-10-26 23:24:59 +0200

    ⚡️ delete bullet when leave screen

--- Assets/Script/BulletBehavior.cs
+++ Assets/Script/BulletBehavior.cs
@@ -6,28 +6,42 @@ public class BulletBehavior : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rb2d;
+
     [SerializeField]
     private int speed;
+
     [SerializeField]
     private int strength;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move up
+        MoveUp();
+        if (transform.position.y > 7)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    private void MoveUp()
+    {
         rb2d.velocity = new Vector2(0, speed);
     }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Enemy")
         {
-            collider.gameObject.GetComponent<EnemyBehavior>().TakeDamage(strength);
-            Destroy(gameObject);
+            collider
+                .gameObject
+                .GetComponent<EnemyBehavior>()
+                .TakeDamage(strength);
+            Destroy (gameObject);
         }
     }
 }
