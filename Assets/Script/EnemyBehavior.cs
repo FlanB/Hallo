ec6bbc75e7288d64ae57bb5d8e57d7a7d01986d3
FlanB <clement.renou@mmibordeaux.com>
2021-10-27 15:15:59 +0200

    change background color on impact

--- Assets/Script/EnemyBehavior.cs
+++ Assets/Script/EnemyBehavior.cs
@@ -16,18 +16,26 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField]
     private int strength;
 
+    void Start()
+    {
+    }
+
     void Update()
     {
         MoveDown();
 
-        // delete gameObject when leaves screen
-        if (transform.position.y < -7 || health <= 0)
+        if (transform.position.y < -5)
+        {
+            StartCoroutine(FlashCamera());
+        }
+        if (health <= 0)
         {
             LifeToZero();
         }
     }
 
-    public virtual void LifeToZero() {
+    public virtual void LifeToZero()
+    {
         Destroy (gameObject);
     }
 
@@ -51,7 +59,7 @@ public class EnemyBehavior : MonoBehaviour
 
         while (DB.getHealth() > 0)
         {
-            DB.TakeDamage(strength);
+            DB.TakeDamage (strength);
             yield return new WaitForSeconds(1);
         }
     }
@@ -59,12 +67,22 @@ public class EnemyBehavior : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-        StartCoroutine(Flash());
+        StartCoroutine(Flash(gameObject.GetComponent<SpriteRenderer>()));
     }
-      private IEnumerator Flash()
+
+    private IEnumerator Flash(SpriteRenderer other)
     {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        other.color = Color.red;
         yield return new WaitForSeconds(0.1f);
-        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        other.color = Color.white;
+
+    }
+
+    private IEnumerator FlashCamera()
+    {
+        Camera.main.backgroundColor = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        Camera.main.backgroundColor = new Color32(49, 121, 99, 255);
+        LifeToZero();
     }
 }
