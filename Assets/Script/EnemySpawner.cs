a63936d6e9b8909b800da1ee709eb50a738b30e6
FlanB <clement.renou@mmibordeaux.com>
2021-10-27 11:31:32 +0200

    ⚡️ better enemies spawn

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -13,12 +13,14 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private int maxEnemies;
 
-    private int spawnRow = 6;
+    private GameObject[] columns;
 
     // Start is called before the first frame update
     void Start()
     {
+        columns = GameObject.FindGameObjectsWithTag("Column");
         StartCoroutine(SpawnEnemy());
+        Debug.Log(columns.Length);
     }
 
     // Update is called once per frame
@@ -30,35 +32,9 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < maxEnemies; i++)
         {
-            // spawn enemy in 5 columns
-            switch (Random.Range(0, 4))
-            {
-                case 0:
-                    Instantiate(enemy,
-                    new Vector2(-2.25f, spawnRow),
-                    Quaternion.identity);
-                    break;
-                case 1:
-                    Instantiate(enemy,
-                    new Vector2(-1.15f, spawnRow),
-                    Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(enemy,
-                    new Vector2(0, spawnRow),
-                    Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(enemy,
-                    new Vector2(1.15f, spawnRow),
-                    Quaternion.identity);
-                    break;
-                case 4:
-                    Instantiate(enemy,
-                    new Vector2(2.25f, spawnRow),
-                    Quaternion.identity);
-                    break;
-            }
+            Instantiate(enemy,
+            columns[Random.Range(0, 4)].transform.position,
+            Quaternion.identity);
 
             yield return new WaitForSeconds(spawnTime);
         }
