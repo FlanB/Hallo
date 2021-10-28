46db251695b9f10d44db669f8c0a17d634206f27
FlanB <clement.renou@mmibordeaux.com>
2021-10-28 11:13:41 +0200

    ✨ new wave system

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -7,29 +7,96 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private float spawnTime;
 
-    [SerializeField]
-    private int maxEnemies;
-
     private GameObject[] columns;
 
     [SerializeField]
-    private GameObject[] enemies;
+    private GameObject[] type1Enemies;
+
+    [SerializeField]
+    private GameObject[] type2Enemies;
+
+    [SerializeField]
+    private GameObject[] type3Enemies;
+
+    [SerializeField]
+    private int incrementWaveStrength;
+
+    private int type1;
+
+    private int type2;
+
+    private int type3;
 
     void Start()
     {
+        type1 = incrementWaveStrength;
+        while (type1 >= 3 || type2 >= 2)
+        {
+            if (type1 >= 3)
+            {
+                type2++;
+                type1 -= 3;
+            }
+            else if (type2 >= 2)
+            {
+                type3++;
+                type2 -= 2;
+            }
+        }
         columns = GameObject.FindGameObjectsWithTag("Column");
         StartCoroutine(SpawnEnemy());
     }
 
     IEnumerator SpawnEnemy()
     {
-        for (int i = 0; i < maxEnemies; i++)
+        while (true)
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)],
-            columns[Random.Range(0, 4)].transform.position,
-            Quaternion.identity);
-
-            yield return new WaitForSeconds(spawnTime);
+            switch (Random.Range(0, 2))
+            {
+                case 0:
+                    if (type1 > 0)
+                    {
+                        type1--;
+                        Instantiate(type1Enemies[Random
+                            .Range(0, type1Enemies.Length)],
+                        columns[Random.Range(0, 4)].transform.position,
+                        Quaternion.identity);
+                    }
+                    else
+                    {
+                        Loop;
+                    }
+                    break;
+                case 1:
+                    if (type2 > 0)
+                    {
+                        type2--;
+                        Instantiate(type2Enemies[Random
+                            .Range(0, type2Enemies.Length)],
+                        columns[Random.Range(0, 4)].transform.position,
+                        Quaternion.identity);
+                    }
+                    else
+                    {
+                        Loop;
+                    }
+                    break;
+                case 2:
+                    if (type3 > 0)
+                    {
+                        type3--;
+                        Instantiate(type3Enemies[Random
+                            .Range(0, type3Enemies.Length)],
+                        columns[Random.Range(0, 4)].transform.position,
+                        Quaternion.identity);
+                    }
+                    else
+                    {
+                        Loop;
+                    }
+                    break;
+            }
         }
+        yield return new WaitForSeconds(spawnTime);
     }
 }
