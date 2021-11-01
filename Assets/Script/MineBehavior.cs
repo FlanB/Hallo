bd404b6da4c02f36bdcf8c00be860f567a69e466
FlanB <clement.renou@mmibordeaux.com>
2021-11-01 01:22:02 +0100

    ✨ Mine explosion

--- Assets/Script/MineBehavior.cs
+++ Assets/Script/MineBehavior.cs
@@ -6,12 +6,23 @@ public class MineBehavior : DefenseBehavior
 {
     [SerializeField]
     private int strength;
+
     private void OnCollisionEnter2D(Collision2D collider)
     {
         if (collider.gameObject.tag == "Enemy")
         {
-            Destroy (gameObject);
-            collider.gameObject.GetComponent<EnemyBehavior>().TakeDamage(strength);
+            gameObject.GetComponent<Animator>().SetTrigger("Explode");
+            collider
+                .gameObject
+                .GetComponent<EnemyBehavior>()
+                .TakeDamage(strength);
+            StartCoroutine(DestroyMine());
         }
+    }
+
+    IEnumerator DestroyMine()
+    {
+        yield return new WaitForSeconds(0.25f);
+        Destroy (gameObject);
     }
 }
